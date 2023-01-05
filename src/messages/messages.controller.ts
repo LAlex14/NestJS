06785ab0276ab14d4aedbfc29c4e7c6db7065638
Lesan Alexandru <alexandru.lesan@binarcode.com>
2023-01-05 23:36:32 +0200

    finished second

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,28 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { CreateMessagesDto } from "./dtos/create-messages.dto";
+import { MessagesService } from "./messages.service";
 
 @Controller('messages')
 export class MessagesController {
+    constructor(public messagesService: MessagesService) {}
+
     @Get()
     listMessages() {
-
+        return this.messagesService.findAll()
     }
 
     // @Query('name') - to access query param
     @Post()
-    createMessages(@Body() body: any) {
-        console.log(body)
-
+    createMessages(@Body() body: CreateMessagesDto) {
+        return this.messagesService.create(body.content)
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        console.log(id)
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id)
+        if(!message) {
+            throw new NotFoundException('message not found')
+        }
+        return message
     }
 }
